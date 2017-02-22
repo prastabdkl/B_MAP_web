18b85ed7772db34d963fa80d7762155939275515
Bipin Manandhar <kubipin07@gmail.com>
2017-02-22 08:43:40 +0545

    update account api update action

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -16,6 +16,9 @@ class Api::V1::AccountController < Api::V1::BaseController
     account = Account.find(params[:id]) if curr_user.is_admin
 
     if account.update(account_params)
+      account.update_attribute(:net_total_addition, account.addition_holiday + account.addition_overtime + account.addition_miscellaneous)
+      account.update_attribute(:net_total_deduction, account.deduction_loan + account.deduction_late + account.deduction_miscellaneous + account.company_deduction_absent + account.salary * account.company_deduction_wtax / 100)
+      account.update_attribute(:net_pay, account.salary + account.net_total_addition - account.net_total_deduction)
       render json: account, status: 200, location: api_v1_account_url
     else
       render json: { errors: account.errors }, status: 422
@@ -24,6 +27,9 @@ class Api::V1::AccountController < Api::V1::BaseController
 
   private
   def account_params
-    params.permit(:post, :salary, :joining_date, :working_plan)
+    params.permit(:post, :salary, :joining_date, :working_plan, :addition_holiday,
+                  :addition_overtime, :addition_miscellaneous, :deduction_loan,
+                  :deduction_late, :deduction_miscellaneous, :company_deduction_absent,
+                  :company_deduction_wtax)
   end
 end
--- app/serializers/api/v1/account_serializer.rb
+++ app/serializers/api/v1/account_serializer.rb
@@ -1,5 +1,9 @@
 class Api::V1::AccountSerializer < Api::V1::BaseSerializer
-  attributes :id, :post, :salary, :joining_date, :working_plan
+  attributes :id, :post, :salary, :joining_date, :working_plan,
+              :addition_holiday, :addition_overtime, :addition_miscellaneous,
+              :deduction_loan, :deduction_late, :deduction_miscellaneous,
+              :company_deduction_absent, :company_deduction_wtax, :net_total_addition,
+              :net_total_deduction, :net_pay
 
   has_one :user
 end
